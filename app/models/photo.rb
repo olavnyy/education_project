aed6c5c09fc13ef422d9458214059056b9f89a71
grek-andrian <gretsko.andrian@gmail.com>
2016-09-30 21:53:32 +0300

    Add photos controller

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,8 +5,8 @@ class Photo < ApplicationRecord
                               default_url: '/images/:style/missing.png'
     validates_attachment :image, presence: true,
                                  content_type: { content_type: 'image/jpg' },
-                                 size: { :in => 0..5000.kilobytes },
-                                 content_type: /^image\/(png|gif|jpeg|jpg)/,
+                                 size: { in: 0..5000.kilobytes },
+                                 content_type: { content_type: /^image\/(png|gif|jpeg|jpg)/ },
                                  message: "Only (png|gif|jpeg|jpg) images are allowed and the size cannot exceed 5Mb"
 
 end
