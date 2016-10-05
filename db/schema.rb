09021578787a84f94e1d7477d9d35b51aa47792a
mstakh <mykola.stakhiv@gmail.com>
2016-10-05 15:04:26 +0300

    Modified schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160930213013) do
+ActiveRecord::Schema.define(version: 20161001083510) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
