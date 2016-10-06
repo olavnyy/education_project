5d6b9f1e28db15868794629eca27c1145d8236a1
lahotslyy <taras.lahotskyy@gmail.com>
2016-10-06 15:42:34 +0300

    Update StudentParent association

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   belongs_to :school
   belongs_to :group
-  has_and_belongs_to_many :parents, join_table: "parents_students"
+  has_and_belongs_to_many :parents, class_name: "Parent"
   has_one :health_info
 
   accepts_nested_attributes_for :health_info
