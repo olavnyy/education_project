ef39b9b432249be6c6cd95aa613999bb364e8450
grek-andrian <gretsko.andrian@gmail.com>
2016-10-08 13:02:50 +0300

    Add serializer for photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,14 @@
-# frozen_string_literal: true
 class PhotosController < ApplicationController
     before_filter :set_album
 
+    def index
+      @photos = Photo.all
+      render json: @photos
+    end
+
     def show
-        @photo = @album.photos.find params[:id]
+        @photo = @album.photos.find(params[:id])
+        render json: @photo
     end
 
     def new
@@ -21,11 +26,11 @@ class PhotosController < ApplicationController
     end
 
     def edit
-        @photo = @album.photos.find params[:id]
+        @photo = @album.photos.find(params[:id])
     end
 
     def update
-        @photo = @album.photos.find params[:id]
+        @photo = @album.photos.find(params[:id])
 
         if @photo.update_attributes params[:photo_params]
             redirect_to @album
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,8 +4,7 @@ class Photo < ApplicationRecord
     has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '100x100>' },
                               default_url: '/images/:style/missing.png'
     validates_attachment :image, presence: true,
-                                 content_type: { content_type: 'image/jpg' },
-                                 size: { in: 0..5000.kilobytes },
+                                 size: { in: 0..2000.kilobytes },
                                  content_type: { content_type: /^image\/(png|gif|jpeg|jpg)/ },
                                  message: "Only (png|gif|jpeg|jpg) images are allowed and the size cannot exceed 5Mb"
 
