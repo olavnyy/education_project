3a9b6d40f11a34fcafaaff9e3185829f9d652d0f
kuriy-yevgeniy <kuriy.yevgeniy@gmail.com>
2016-10-10 14:04:08 +0300

    LWR-173 AMS for Schools

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,30 @@ User.create(first_name: 'Super',
             type: 'Superadmin',
             contact_phone: '+123456789012')
 
+Group.create(name: "a")
+
+Album.create(title: "Dogs",
+            image: "dog.png",
+            imageable_type: "Group",
+            imageable_id: 1)
+
+Album.create(title: "Horses",
+            image: "horse.jpg",
+            imageable_type: "Group",
+            imageable_id: 1)
+
+5.times do |n|
+  FactoryGirl.create(:level)
+end
+
+5.times do |n|
+  FactoryGirl.create(:group)
+end
+
+5.times do |n|
+  FactoryGirl.create(:news)
+end
+
 5.times do |n|
 	FactoryGirl.create(:school)
 end
