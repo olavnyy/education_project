56be1902b9d5d502d30d0630e8abc99c350b6382
grek-andrian <gretsko.andrian@gmail.com>
2016-10-10 10:39:07 +0300

    LWR-192 Create ActiveModelSerializers + tests for Photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,8 @@
 class Photo < ApplicationRecord
     belongs_to :album
     has_attached_file :image, styles: { large: '600x600>', medium: '300x300>', thumb: '100x100>' },
-                              default_url: '/images/:style/missing.png'
+                              path: ":rails_root/public/images/:id/:filename",
+                              url: "/images/:id/:filename"
     validates_attachment :image, presence: true,
                                  size: { in: 0..2000.kilobytes },
                                  content_type: { content_type: /^image\/(png|gif|jpeg|jpg)/ },
--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,15 @@ Rails.application.routes.draw do
   resources :schools, defaults: { format: 'json' }
   resources :news, defaults: { format: 'json' }
   resources :groups, :levels, defaults: { format: 'json' }
+  resources :teachers, defaults: { format: 'json' }
+  resources :parents, defaults: { format: 'json' }
+  resources :students, defaults: { format: 'json' }
 
   devise_for :users
-    devise_scope :user do
-      authenticated :user do
-        root 'home#index', as: :authenticated_root
-        resources :teachers, defaults: { format: 'json' }
-        resources :parents, defaults: { format: 'json' }
-        resources :students, defaults: { format: 'json' }
-      end
-      unauthenticated do
-        root 'devise/sessions#new', as: :unauthenticated_root
-      end
-    end
+
 # Routes for photoalbums
   resources :albums do
-    resources :photos do
-    end
+    resources :photos
   end
 
   get 'album/new', to: 'albums#new'
