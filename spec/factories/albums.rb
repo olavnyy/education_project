29d204b678852ac827cfec84416c306dcca0b502
zoryana94 <zoryanavakh@gmail.com>
2016-10-19 16:10:03 +0300

    Add some factories

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :album do
-    title "Title"
-    image "image.png"
+    sequence :title do |n|
+      "Album #{n}"
+    end
+    image "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQSpyafEqJwu7iXv3deAkDbyZ-wklcQlAxGxei-Z8kWKEjeDvOVhA"
+    sequence :imageable_id do |n|
+      "#{n}"
+    end
+    imageable_type "Group"
   end
 end
 
