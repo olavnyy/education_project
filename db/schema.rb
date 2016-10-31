5cb88c01841ccdc137d43ae67da2ede96b3da233
lahotskyy <taras.lahotskyy@gmail.com>
2016-11-01 01:25:10 +0200

    fix conflict in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031201628) do
+ActiveRecord::Schema.define(version: 20161030104323) do
 
   create_table "albums", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
--- spec/factories/news.rb
+++ spec/factories/news.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :news do
     sequence(:title) { |n| "Title  #{n}" }
     sequence(:description) { |n| "Description #{n}" }
-    viewable_type "School"
-    sequence(:viewable_id ) { |n| "#{n}" }
+    imageable_type "School"
+    sequence(:imageable_id ) { |n| "#{n}" }
   end
 end
