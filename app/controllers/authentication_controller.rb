929e1faa61c4cb78f7439a9c1cada29ba881239b
Genya <Yevhen.Shabala@gmail.com>
2016-11-01 16:18:03 +0200

    fixing type

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -14,7 +14,7 @@ class AuthenticationController < ApplicationController
     return nil unless user and user.id
     {
       auth_token: JsonWebToken.encode({user_id: user.id}),
-      user: {id: user.id, email: user.email}
+      user: {id: user.id, email: user.email, type: user.type }
     }
   end
 end
