d908fc4ff1fc234540a5d3ea24be54b54ab98ff0
zoryana94 <zoryanavakh@gmail.com>
2016-11-09 21:34:48 +0200

    Resolve the conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109170548) do
+
+ActiveRecord::Schema.define(version: 20161104223534) do
+
 
   create_table "albums", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
