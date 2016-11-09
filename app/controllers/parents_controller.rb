a0cf9c6031273097177bd3208b4f6996c21e00f3
kuriy-yevgeniy <kuriy.yevgeniy@gmail.com>
2016-11-10 00:15:14 +0200

    Parents CRUD

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,68 +1,57 @@
 class ParentsController < ApplicationController
   load_and_authorize_resource
+  before_action :set_parent, only: [:show, :update, :destroy]
+
+  def index
+    render_content(parents_list)
+  end
 
   def show
-    @parent = @current_user.class.parents_list(@current_user).find(params[:id])
-    render json: @parent
-  end
-
-  def new
-    @parent = @current_user.class.parents_list(@current_user)
-    render json: @parent
-  end
-
-   def index
-    @parent = @current_user.class.parents_list(@current_user)
-    render json: @parent
+    render_content(@parent)
   end
 
   def create
     @parent = Parent.new(parent_params)
-    @parent.school_id = @current_user.school_id
+    @teacher.school_id = @current_user.school_id
     render_content(@parent.save ? {parent: @parent, status: true} : {errors: @parent.errors, status: false})
   end
 
-  def edit
-    @parent= @current_user.class.parents_list(@current_user).find(params[:id])
-    render json: @parent
+  def update
+    render_content(@parent.update_attributes(parent_update_params) ? {parent: @parent, status: true} : {errors: @parent.errors, status: false})
   end
 
   def destroy
-    @current_user.class.parents_list(@current_user).find(params[:id]).destroy
-    render json: {
-      content: 'deleted'
-    }
-  end
-
-  def update
-    @parent = @current_user.class.parents_list(@current_user).find(params[:id])
-    if @parent.update_attributes(parent_params)
-     render json: @parent
-    # Handle a successful update.
-    else
-      render json: {
-        content: 'invalid update'
-      }
-    end
+    render_content({status: (@parent && @parent.destroy ? true : false)})
   end
 
   private
 
+  def set_parent
+    @parent = parents_list.find_by(id: params[:id])
+  end
+
   def parents_list
-    @current_user.type?('Admin') ? @current_user.school.parents : @current_user.parents
+    @current_user.type?('Admin') ? @current_user.school.parents : @current_user.students.parents
   end
 
   def parent_params
     merge_params
-      params
-        .require(:parent)
-        .permit(:first_name, :last_name, :email,:contact_phone, 
-                :password, :password_confirmation, :school_id, :student_id)
+    params
+      .require(:parent)
+      .permit(:first_name, :last_name, :email, :contact_phone,
+             :password, :password_confirmation, :school_id, :student_id)
   end
 
   def merge_params
-      params[:parent][:password] = params[:password]
-      params[:parent][:password_confirmation] = params[:password_confirmation]
+    params[:parent][:password] = params[:password]
+    params[:parent][:password_confirmation] = params[:password_confirmation]
+  end
+
+  def parent_update_params
+    params
+      .require(:user)
+      .permit(:first_name, :last_name, :email, :contact_phone,
+              :school_id, :student_id)
   end
 
 end
