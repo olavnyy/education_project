6dc50b2ae6894b5d996f41589c31e73bfed61fd7
zoryana94 <zoryanavakh@gmail.com>
2016-11-15 20:46:35 +0200

    Add the models MyDay and OurDay

--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -2,4 +2,6 @@ class DailyReport < ApplicationRecord
   belongs_to :group
   belongs_to :student
   has_many :attendances, dependent: :destroy
+  has_many :my_days, dependent: :destroy
+  has_one :our_day, dependent: :destroy
 end
