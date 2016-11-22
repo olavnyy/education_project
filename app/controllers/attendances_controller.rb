edc6d02887b7a1f02bf01f8ed4752731be5ccfe5
Yevhen <Yevhen.Shabala@gmail.com>
2016-11-22 02:00:00 +0200

    Journal refactor (#86)
    * Refactoring creating attendances and daily reports
    
    * Refactor attendanc
    
    * Base refactor
    
    * Refactoring all

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,8 +1,8 @@
 class AttendancesController < ApplicationController
   load_and_authorize_resource
   before_action :set_attendance, only: [:show, :update]
-  before_action :set_attendances, only: [:index]
-  # after_action :update_time
+  before_action :all_attendances, only: [:index]
+
 
   def index
     render_content(@attendances)
@@ -14,26 +14,8 @@ class AttendancesController < ApplicationController
 
   def update
     update_time(params)
-    if check_for_report_times(params[:id])
-      @report_time = ReportTime.where(attendance_id: params[:id]).last
-    else
-      @report_time = ReportTime.new
-    end
-    if @report_time.start and !@report_time.end and (server_time > @report_time.start)
-      report_time_update = {
-          end: server_time
-      }
-      @report_time.update_attributes(report_time_update)
-    elsif (@report_time.start and @report_time.end) or (!@report_time.start and !@report_time.end)
-      @report_time = ReportTime.new
-      @report_time.start = server_time
-      @report_time.attendance_id = params[:id]
-      @report_time.save
-    end
-    attendance_params_update = {
-        present:  attendance_params[:present]
-    }
-    render_content(@attendance.update_attributes(attendance_params_update) ? {attendance: @attendance, status: true} : {errors: @attendance.errors, status: false})
+    set_attendance
+    render_content(@attendance)
   end
 
   private
@@ -42,17 +24,25 @@ class AttendancesController < ApplicationController
     @attendance = attendances_list.find_by(id: params[:id])
   end
 
-  def set_attendances
+  def all_attendances
     @attendances = []
     @current_user.students.each do |student|
       @attendances << student.attendances.find_or_create_by(time: server_day, group: student.group)
     end
   end
 
-  # update/create time for current attendance
-  # def update_time(params)
-  #
-  # end
+  def update_time(params)
+    if !params[:present] and exists_report_time(params[:id])
+      @report_time = ReportTime.where(attendance_id: params[:id]).last
+      @report_time.update_attributes(end: server_time) if server_time > @report_time[:start]
+    else
+      @report_time = ReportTime.new
+      @report_time.start = server_time
+      @report_time.attendance_id = params[:id]
+      @report_time.save
+    end
+    @attendance.update_attributes(present: params[:present])
+  end
 
   def attendances_list
     @current_user.type?('Teacher') ? @current_user.group.attendances : @current_user.attendances
@@ -60,16 +50,6 @@ class AttendancesController < ApplicationController
 
   def check_for_report_times(id)
     ReportTime.find_by(attendance_id: id)
-  end
-
-  def server_day
-    t = Time.now
-    t.strftime("%Y-%m-%d")
-  end
-
-  def server_time
-    t = Time.now
-    t.strftime("%H:%M:%S")
   end
 
   def attendance_params
