507d7397057688e0c042c5bd634a227e523257e4
Genya <Yevhen.Shabala@gmail.com>
2016-11-23 16:43:55 +0200

    Refactor daily reports

--- db/migrate/20161109145608_add_student_index_to_daily_reports.rb
+++
@@ -1,6 +0,0 @@
-class AddStudentIndexToDailyReports < ActiveRecord::Migration[5.0]
-  def change
-    add_column :daily_reports, :student_id, :integer
-    add_index :daily_reports, :student_id
-  end
-end
