5db066ef217cf514640c572a966dc77ba11e5f5d
lahotskyy <taras.lahotskyy@gmail.com>
2016-11-26 05:26:06 +0200

    implemented  associations Parents-Students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,12 +14,14 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(student_params)
     add_avatar_and_health_info
+    set_parents
     @student.write_attribute(:school_id, @current_user.school_id)
     render_content(@student.save ? {student: @student, status: true} : {errors: @student.errors, status: false})
   end
 
   def update
     update_avatar_and_health_info
+    binding.pry
     render_content(@student.update_attributes(student_params) ? {student: @student, status: true} : {errors: @student.errors, status: false})
   end
 
@@ -49,10 +51,23 @@ class StudentsController < ApplicationController
   def update_avatar_and_health_info
     @student.avatar = decode_base64_image(params[:student][:avatar]) if params[:student][:avatar] && @student
     if  params[:student][:health_info]
+      @student.health_info ||= HealthInfo.new
       @student.health_info.description = params[:student][:health_info][:description]
       @student.health_info.health_problem = params[:student][:health_info][:health_problem]
     end
   end
+
+  def set_parents
+    if params[:parents][:first]
+      @parentFirstId = params[:parents][:first][:id]
+      @student.parents << Parent.find_by(id: @parentFirstId)
+    end
+    if params[:parents][:second]
+      @parentSecondId = params[:parents][:second][:id]
+      @student.parents << Parent.find_by(id: @parentSecondId)
+    end
+  end
+
 
   def student_params
     params
