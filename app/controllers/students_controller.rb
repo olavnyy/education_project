cf44e00720d30ccb6b717b26048c5bbd4416fccd
lahotskyy <taras.lahotskyy@gmail.com>
2016-11-26 03:22:58 +0200

    update HealthInfo

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,15 +13,13 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new(student_params)
-    add_avatar
-    binding.pry
-    @student.school_id = @current_user.school_id
+    add_avatar_and_health_info
+    @student.write_attribute(:school_id, @current_user.school_id)
     render_content(@student.save ? {student: @student, status: true} : {errors: @student.errors, status: false})
   end
 
   def update
-    update_avatar
-    binding.pry
+    update_avatar_and_health_info
     render_content(@student.update_attributes(student_params) ? {student: @student, status: true} : {errors: @student.errors, status: false})
   end
 
@@ -39,17 +37,27 @@ class StudentsController < ApplicationController
     @current_user.type?('Admin') ? @current_user.school.students : @current_user.students
   end
 
-  def add_avatar
+  def add_avatar_and_health_info
     @student.avatar = decode_base64_image(params[:avatar]) if params[:avatar] && @student
+    if  params[:health_info]
+      @student.health_info = HealthInfo.new
+      @student.health_info.description = params[:health_info][:description]
+      @student.health_info.health_problem = params[:health_info][:health_problem]
+    end
   end
 
-  def update_avatar
+  def update_avatar_and_health_info
     @student.avatar = decode_base64_image(params[:student][:avatar]) if params[:student][:avatar] && @student
+    if  params[:student][:health_info]
+      @student.health_info.description = params[:student][:health_info][:description]
+      @student.health_info.health_problem = params[:student][:health_info][:health_problem]
+    end
   end
 
   def student_params
     params
       .require(:student)
-      .permit(:first_name, :last_name, :group_id, :school_id, :age, :avatar, :health_info)
+      .permit(:first_name, :last_name, :group_id, :school_id, :age, :avatar,
+              :health_info)
   end
 end
