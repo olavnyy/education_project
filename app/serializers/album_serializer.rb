771bdd6843020cd131b7878bfa8251a84a5c0098
grek-andrian <gretsko.andrian@gmail.com>
2016-11-27 21:10:47 +0200

    Add album cover in album_serializer

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,7 +1,11 @@
 class AlbumSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :title, :image, :created_at, :imageable_type, :imageable_id
+  attributes :id, :title, :image, :created_at, :imageable_type, :imageable_id, :album_cover
   has_many :photos
+
+  def album_cover
+      object.photos.first.image.url(:large)
+  end
 
 end
